9d9ceef3201ba26c32c113c642091150febe37a9
EC2 Default User <ec2-user@ip-172-31-45-139.ap-south-1.compute.internal>
2023-03-07 13:04:15 +0000

    added server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+import http from 'http';
+import express from 'express';
+import path from 'path';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __dirname = path.resolve();
+
+app.get('*', (req, res) =>
+  res.send('finally got there! yeahaey!!')
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+
+const httpServer = http.Server(app);
+
+httpServer.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
+
